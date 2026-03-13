000f173234fe5b6954c3071cf07dad824fd0ba13
Nityom <nityomtikherr@gmail.com>
2026-03-13 22:54:23 +0530

    chore: update devDependencies for eslint and add lodash and eslint-config-next

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,6 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import eslintrc from "@eslint/eslintrc";
-
-const { FlatCompat } = eslintrc;
+import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -13,6 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/ban-ts-comment": "warn",
+      "react/no-unescaped-entities": "warn",
+      "prefer-const": "warn",
+    },
+  },
 ];
 
 export default eslintConfig;
